0f8d8a8eb8b1e0c4ee5544bc278db9fd75e99faa
roverchen <rover.k.chen@gmail.com>
2021-09-09 20:55:48 +0800

    </select>

--- includes/class-users.php
+++ includes/class-users.php
@@ -57,13 +57,21 @@ if (!class_exists('users')) {
                 $output .= '</tbody></table></figure>';
 
                 $output .= '<figure class="wp-block-table"><table><tbody>';
-                $output .= '<tr><td>'.'#'.'</td><td>'.'Courses'.'</td></tr>';
+                $output .= '<tr><td>'.'#'.'</td><td>'.'Courses'.'</td><td>Lecturer</td><td>Witness</td><td>Date</td></tr>';
                 global $wpdb;
                 $results = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}user_courses WHERE student_id = {$_GET['_id']}", OBJECT );
                 foreach ($results as $index => $result) {
-                    $output .= '<tr><td>'.$index.'</td><td>'.'<select name="_course_id_'.$index.'">'.Courses::select_options($results[$index]->course_id).'</td></tr>';
+                    $output .= '<tr><td>'.$index.'</td>';
+                    $output .= '<td><select name="_course_id_'.$index.'">'.Courses::select_options($results[$index]->course_id).'</select></td>';
+                    $output .= '<td><select name="_lecturer_id_'.$index.'">'.self::select_options($results[$index]->lecturer_id).'</select></td>';
+                    $output .= '<td></td>';
+                    $output .= '<td></td></tr>';
                 }
-                $output .= '<tr><td>'.($index+1).'</td><td>'.'<select name="_course_id">'.Courses::select_options().'</select>'.'</td></tr>';
+                $output .= '<tr><td>'.($index+1).'</td>';
+                $output .= '<td><select name="_course_id">'.Courses::select_options().'</select></td>';
+                $output .= '<td><select name="_lecturer_id">'.self::select_options().'</select></td>';
+                $output .= '<td></td>';
+                $output .= '<td></td></tr>';
                 $output .= '</tbody></table></figure>';
                 
                 $output .= '<div class="wp-block-buttons">';
