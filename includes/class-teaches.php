99a072f7e054c8bd52a523a82ff1f617a77df695
roverchen <rover.k.chen@gmail.com>
2021-09-09 09:28:31 +0800

    {$_GET['_id']}

--- includes/class-teaches.php
+++ includes/class-teaches.php
@@ -44,7 +44,7 @@ if (!class_exists('teaches')) {
 
                 $output .= '<figure class="wp-block-table"><table><tbody>';
                 $output .= '<tr><td>'.'#'.'</td><td>'.'Courses'.'</td></tr>';
-                $results = $wpdb->get_row( "SELECT * FROM {$wpdb->prefix}teach_courses WHERE teach_id = {$_GET['_id']}", OBJECT );
+                $results = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}teach_courses WHERE teach_id = {$_GET['_id']}", OBJECT );
                 foreach ($results as $index => $result) {
                     $output .= '<tr><td>'.$index.'</td><td>'.'<select name="_course_id'.$index.'">'.Courses::select_options($results[$index]->course_id).'</td></tr>';
                 }
