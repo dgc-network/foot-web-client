89e82659899da4020c5d0ece721bf5b7a34560e8
roverchen <rover.k.chen@gmail.com>
2021-09-11 09:57:04 +0800

    include_once dirname( __FILE__ ) . '/includes/class-teaches.php';

--- foot-reflexology.php
+++ foot-reflexology.php
@@ -18,8 +18,9 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 include_once dirname( __FILE__ ) . '/includes/class-courses.php';
-include_once dirname( __FILE__ ) . '/includes/class-teaches.php';
 include_once dirname( __FILE__ ) . '/includes/class-users.php';
+include_once dirname( __FILE__ ) . '/includes/class-learnings.php';
+include_once dirname( __FILE__ ) . '/includes/class-teaches.php';
 /*
 include_once dirname( __FILE__ ) . '/php-OP_RETURN/OP_RETURN.php';
 include_once dirname( __FILE__ ) . '/vendor/autoload.php';
