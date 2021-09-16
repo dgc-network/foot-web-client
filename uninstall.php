d318a40b4825d0b3f155c35fcd95c00dd0130dd9
Rover Chen <rover.k.chen@gmail.com>
2021-09-16 16:14:12 +0800

    //$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}user_course_learnings" );

--- uninstall.php
+++ uninstall.php
@@ -4,6 +4,7 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
     die;
 }
 global $wpdb;
+$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}course_lecturers_witnesses" );
 //$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}user_course_learnings" );
 //$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}course_learnings" );
 /*
