d3c25dc9dfb27a2423b77cabf0564585a8e480f6
Rover Chen <rover.k.chen@gmail.com>
2021-09-16 15:14:57 +0800

    $_id,

--- includes/class-courses.php
+++ includes/class-courses.php
@@ -69,7 +69,7 @@ if (!class_exists('courses')) {
             $row = $wpdb->get_row( "SELECT * FROM {$wpdb->prefix}courses WHERE course_id = {$_id}", OBJECT );
             $CreateDate = wp_date( get_option( 'date_format' ), $row->created_date );
             $current_user_id = get_current_user_id();
-            //$repack = self::repack_lecturers_witnesses();
+            $repack = self::repack_lecturers_witnesses();
             $output  = '<form method="post">';
             $output .= '<figure class="wp-block-table"><table><tbody>';
             $output .= '<tr><td>'.'Name:'.'</td><td>'.get_userdata($current_user_id)->display_name.'</td></tr>';
@@ -87,15 +87,15 @@ if (!class_exists('courses')) {
             foreach ($results as $index => $result) {
                 $output .= '<tr><td>'.$index.'</td>';
                 $output .= '<td>'.'<select name="_learning_id_'.$index.'">'.self::select_learnings($_id, $results[$index]->learning_id).'</select></td>';
-                $output .= '<td>'.'<select name="_lecturer_witness_id_'.$index.'">'.Users::select_options($results[$index]->lecturer_witness_id).'</select></td>';
-                //$output .= '<td>'.'<select name="_lecturer_witness_id_'.$index.'">'.self::select_lecturers_witnesses($results[$index]->lecturer_witness_id).'</select></td>';
+                //$output .= '<td>'.'<select name="_lecturer_witness_id_'.$index.'">'.Users::select_options($results[$index]->lecturer_witness_id).'</select></td>';
+                $output .= '<td>'.'<select name="_lecturer_witness_id_'.$index.'">'.self::select_lecturers_witnesses($_id, $results[$index]->lecturer_witness_id).'</select></td>';
                 //$ExpireDate = wp_date( get_option( 'date_format' ), $results[$index]->expired_date );
                 //$output .= '<td><input type="text" name="_expired_date_'.$index.'" value="'.$ExpireDate.'">'.'</td></tr>';
             }
             $output .= '<tr><td>'.($index+1).'</td>';
             $output .= '<td>'.'<select name="_learning_id">'.self::select_learnings($_id).'</select>'.'</td>';
-            $output .= '<td>'.'<select name="_lecturer_witness_id">'.Users::select_options().'</select>'.'</td>';
-            //$output .= '<td>'.'<select name="_lecturer_witness_id">'.self::select_lecturers_witnesses().'</select>'.'</td>';
+            //$output .= '<td>'.'<select name="_lecturer_witness_id">'.Users::select_options().'</select>'.'</td>';
+            $output .= '<td>'.'<select name="_lecturer_witness_id">'.self::select_lecturers_witnesses($_id).'</select>'.'</td>';
             //$output .= '<td><input type="date" name="_expired_date"></td></tr>';
             $output .= '</tbody></table></figure>';
             
