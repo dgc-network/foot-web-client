5c4f9bf6acaca10525cd9fb4c78af7734f2bbd50
roverchen <rover.k.chen@gmail.com>
2021-09-22 14:46:16 +0800

    $funded_txn

--- php-OP_RETURN/OP_RETURN.php
+++ php-OP_RETURN/OP_RETURN.php
@@ -388,6 +388,8 @@
 	
 	function OP_RETURN_sign_send_txn($raw_txn, $testnet=false) {
 	
+		$funded_txn=OP_RETURN_bitcoin_cmd('fundrawtransaction', $testnet, $raw_txn);
+		return $funded_txn;
 		$params = array("mypassphrase", 60);
 		$signed_txn=OP_RETURN_bitcoin_cmd('walletpassphrase', $testnet, $params);
 
