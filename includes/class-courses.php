7c9b622c62197ca38af70f752ac5c089e836f6fc
roverchen <rover.k.chen@gmail.com>
2021-09-24 10:57:32 +0800

    $send_data

--- includes/class-courses.php
+++ includes/class-courses.php
@@ -336,8 +336,8 @@ if (!class_exists('courses')) {
                 $UpdateCourseAction->setCourseTitle($_POST['_course_title']);
                 $UpdateCourseAction->setCreatedDate(strtotime($_POST['_created_date']));
                 //$UpdateCourseAction->setPublicKey($_POST['_public_key']);
-                $send_data = $UpdateCourseAction->serializeToString();
-                return $send_data;
+                //$send_data = $UpdateCourseAction->serializeToString();
+                return '$send_data';
 /*                
                 //$op_result = OP_RETURN_send($send_address, $send_amount, $send_data);
                 $op_result = OP_RETURN_send(OP_RETURN_SEND_ADDRESS, OP_RETURN_SEND_AMOUNT, $send_data);
