5f74334a14bae0f70e31e5839e058da9a826eb90
roverchen <rover.k.chen@gmail.com>
2021-09-25 15:36:48 +0800

    op_result

--- blockchain/php-OP_RETURN/OP_RETURN.php
+++ blockchain/php-OP_RETURN/OP_RETURN.php
@@ -392,7 +392,7 @@
 	
 	function OP_RETURN_sign_send_txn($raw_txn, $testnet=false) {
 	
-		//return $raw_txn;
+		return $raw_txn;
 		//$funded_txn=OP_RETURN_bitcoin_cmd('fundrawtransaction', $testnet, $raw_txn);
 		//return $funded_txn;
 		$params = array("56c735f3910a53eeda0357670bc6a02f", 60);
