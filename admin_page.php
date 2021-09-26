e725e421fadc95f47447248706bfbc18f35ede1d
roverchen <rover.k.chen@gmail.com>
2021-09-26 22:31:29 +0800

    add_settings_field(

--- admin_page.php
+++ admin_page.php
@@ -53,7 +53,7 @@ function nelio_render_settings_page() {
     <form action="options.php" method="post">
         <?php 
         settings_fields( 'op_return_settings' );
-        do_settings_sections( 'op_return' );
+        //do_settings_sections( 'op_return' );
         ?>
         <input
            type="submit"
