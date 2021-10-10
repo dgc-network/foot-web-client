02ed7d8a8001c64f7943ed00d1109114f98ae20c
roverchen <rover.k.chen@gmail.com>
2021-10-10 20:09:09 +0800

    class="wp-block-button__link"

--- includes/class-courses.php
+++ includes/class-courses.php
@@ -104,7 +104,7 @@ if (!class_exists('courses')) {
             $output .= '<div class="wp-block-button">';
             $output .= '<input class="wp-block-button__link" type="submit" value="Submit" name="submit_action">';
             $output .= '</div>';
-            //$output .= '</form>';
+            $output .= '</form>';
             //$output .= '<form method="get">';
             $output .= '<div class="wp-block-button">';
             //$output .= '<input class="wp-block-button__link" type="submit" value="Cancel"';
@@ -112,7 +112,7 @@ if (!class_exists('courses')) {
             //$output .= '<a href="javascript:history.go(-1)">Back</a>';
             $output .= '</div>';
             $output .= '</div>';
-            $output .= '</form>';
+            //$output .= '</form>';
 
             return $output;
         }
