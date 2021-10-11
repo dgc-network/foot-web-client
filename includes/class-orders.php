3515aa7d8dac30d6fe53c90159a286357f412bc9
roverchen <rover.k.chen@gmail.com>
2021-10-11 18:35:52 +0800

    return $output;

--- includes/class-orders.php
+++ includes/class-orders.php
@@ -212,6 +212,7 @@ if (!class_exists('orders')) {
                 'post_status' => array_keys( wc_get_order_statuses() ),
             ) );
             $output  = '<h2>訂單列表</h2>';
+            return $output;
             foreach ($customer_orders as $order_object){
                 foreach ($order_object as $order_id){
                     //return var_dump($order_id);
