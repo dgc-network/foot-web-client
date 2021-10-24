c5165a42ea611e59082c115ef0de761922cb7d6b
roverchen <rover.k.chen@gmail.com>
2021-10-24 21:49:39 +0800

    return $output;

--- includes/class-certifications.php
+++ includes/class-certifications.php
@@ -308,7 +308,7 @@ if (!class_exists('certifications')) {
                 $results = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}course_learnings WHERE course_id = {$product->get_id()}", OBJECT );
                 $output .= '<figure class="wp-block-table"><table><tbody>';
                 foreach ($results as $index => $result) {
-                    $output .= '<td><a href="?edit_mode=true&_course_id='.$product->get_id().'&_id='.$results[$index]->learning_id.'">'.$results[$index]->learning_title.'('.$results[$index]->learning_id.')</a></td>';
+                    $output .= '<tr><td><a href="?edit_mode=true&_course_id='.$product->get_id().'&_id='.$results[$index]->learning_id.'">'.$results[$index]->learning_title.'('.$results[$index]->learning_id.')</a></td></tr>';
                     return $output;
                     $c_results = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}course_learnings WHERE teaching_id = {$results[$index]->learning_id}", OBJECT );
                     foreach ($c_results as $c_index => $c_result) {
