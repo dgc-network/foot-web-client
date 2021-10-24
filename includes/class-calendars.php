711d827ca8061261f834ee7183ca60f21f8be014
roverchen <rover.k.chen@gmail.com>
2021-10-24 15:03:18 +0800

    my_calendars

--- includes/class-calendars.php
+++ includes/class-calendars.php
@@ -22,233 +22,9 @@ if (!class_exists('calendars')) {
             ) );
         }
 
-        function course_learnings( $_id=null ) {
-
-            if ($_id==null){
-                return '<div>course ID is required</div>';
-            }
-
-            if( isset($_POST['submit_action']) ) {        
-                /** 
-                 * submit
-                 */
-                $current_user_id = get_current_user_id();
-                global $wpdb;
-                $results = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}user_course_learnings WHERE student_id = {$current_user_id} AND course_id = {$_id}", OBJECT );
-                foreach ($results as $index => $result) {
-                    if (( $_POST['_learning_id_'.$index]=='select_delete' ) || ( $_POST['_lecturer_witness_id_'.$index]=='select_delete' ) ){
-                        $table = $wpdb->prefix.'user_course_learnings';
-                        $where = array(
-                            'u_c_l_id' => $results[$index]->u_c_l_id
-                        );
-                        $wpdb->delete( $table, $where );    
-                    } else {
-                        $table = $wpdb->prefix.'user_course_learnings';
-                        $data = array(
-                            'learning_id' => $_POST['_learning_id_'.$index],
-                            'lecturer_id' => $_POST['_lecturer_id_'.$index],
-                            'witness_id' => $_POST['_witness_id_'.$index],
-                        );
-                        $where = array(
-                            'u_c_l_id' => $results[$index]->u_c_l_id
-                        );
-                        $wpdb->update( $table, $data, $where );    
-                    }
-                }
-                if ( !($_POST['_learning_id']=='no_select') ){
-                    $table = $wpdb->prefix.'user_course_learnings';
-                    $data = array(
-                        'student_id' => $current_user_id,
-                        'learning_id' => $_POST['_learning_id'],
-                        'lecturer_id' => $_POST['_lecturer_id'],
-                        'witness_id' => $_POST['_witness_id'],
-                        'course_id' => $_id,
-                    );
-                    $format = array('%d', '%d', '%d', '%d', '%d');
-                    $wpdb->insert($table, $data, $format);
-                }
-            }
-
-            /** 
-             * course_learnings header
-             */
-            $current_user_id = get_current_user_id();
-            $product = wc_get_product( $_id );
-
-            $output  = '<h2>個人學習項目的輔導與認證</h2>';
-            $output .= '<form method="post">';
-            $output .= '<figure class="wp-block-table"><table><tbody>';
-            $output .= '<tr><td>'.'Name:'.'</td><td>'.get_userdata($current_user_id)->display_name.'</td></tr>';
-            $output .= '<tr><td>'.'Email:'.'</td><td>'.get_userdata($current_user_id)->user_email.'</td></tr>';
-            $output .= '<tr><td>'.'Title:'.'</td><td>'.$product->get_name().'</td></tr>';
-            $output .= '</tbody></table></figure>';
-            //return $output;
-
-            /** 
-             * user course relationship with learning
-             */
-            global $wpdb;
-            $results = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}user_course_learnings WHERE student_id = {$current_user_id} AND course_id = {$_id}", OBJECT );
-            if (empty($results)) {
-                $c_results = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}course_learnings WHERE course_id = {$_id}", OBJECT );
-                foreach ($c_results as $index => $result) {
-                    $table = $wpdb->prefix.'user_course_learnings';
-                    $data = array(
-                        'student_id' => $current_user_id,
-                        'learning_id' => $c_results[$index]->learning_id,
-                        'course_id' => $_id,
-                        //'lecturer_id' => $_POST['_lecturer_id'],
-                        //'witness_id' => $_POST['_witness_id'],
-                    );
-                    //$format = array('%d', '%d', '%d', '%d', '%d');
-                    $format = array('%d', '%d', '%d');
-                    $wpdb->insert($table, $data, $format);
-                }
-                $results = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}user_course_learnings WHERE student_id = {$current_user_id} AND course_id = {$_id}", OBJECT );
-            }
-            $output .= '<figure class="wp-block-table"><table><tbody>';
-            $output .= '<tr><td>'.'#'.'</td><td>Learnings</td><td>Lecturers</td><td>Witnesses</td></tr>';
-            foreach ($results as $index => $result) {
-                $output .= '<tr><td>'.($index+1).'</td>';
-                //$output .= '<td>'.'<select name="_learning_id_'.$index.'">'.courses::select_learnings($_id, $results[$index]->learning_id).'</select></td>';
-                $row = $wpdb->get_row( "SELECT * FROM {$wpdb->prefix}course_learnings WHERE learning_id = {$results[$index]->learning_id}", OBJECT );
-                $output .= '<td><a href="'.$row->learning_link.'">'.$row->learning_title.'</a></td>';
-                $output .= '<td>'.'<select name="_lecturer_id_'.$index.'">'.courses::select_lecturers($results[$index]->learning_id, $results[$index]->lecturer_id).'</select></td>';
-                $output .= '<td>'.'<select name="_witness_id_'.$index.'">'.courses::select_witnesses($results[$index]->learning_id, $results[$index]->witness_id).'</select></td>';
-                $output .= '</tr>';
-            }
-/*            
-            $output .= '<tr><td>'.'#'.'</td>';
-            $output .= '<td>'.'<select name="_learning_id">'.courses::select_learnings($_id).'</select>'.'</td>';
-            $output .= '<td></td><td></td>';
-*/            
-            $output .= '</tbody></table></figure>';
-            
-            /** 
-             * course_learnings footer
-             */
-            $output .= '<div class="wp-block-buttons">';
-            $output .= '<div class="wp-block-button">';
-            $output .= '<input class="wp-block-button__link" type="submit" value="Submit" name="submit_action">';
-            $output .= '</div>';
-            $output .= '</form>';
-            $output .= '<form method="get">';
-            $output .= '<div class="wp-block-button">';
-            $output .= '<input class="wp-block-button__link" type="submit" value="Cancel"';
-            $output .= '</div>';
-            $output .= '</div>';
-            $output .= '</form>';
-
-            return $output;
-        }
-
-        function view_mode($_id=null) {
-
-            if ($_id==null){
-                $_id=get_current_order_id();
-            }
-
-            if( isset($_POST['submit_action']) ) {
-        
-                global $wpdb;
-                /** 
-                 * submit the order relationship with course learning
-                 */
-                $results = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}course_learnings WHERE course_id = {$_id}", OBJECT );
-                foreach ($results as $index => $result) {
-
-                    $op_result = OP_RETURN_send(OP_RETURN_SEND_ADDRESS, OP_RETURN_SEND_AMOUNT, $send_data);
-                    //return var_dump($op_result);
-                
-                    if (isset($op_result['error'])) {
-
-                        $result_output = 'Error: '.$op_result['error']."\n";
-                        return $result_output;
-                    }
-                    else {
-                        //$result_output = 'TxID: '.$op_result['txid']."\nWait a few seconds then check on: http://coinsecrets.org/\n";
-
-                        global $wpdb;
-                        $table = $wpdb->prefix.'order_course_learnings';
-                        $data = array(
-                            'learning_date' => strtotime($_POST['_learning_date_'.$index]), 
-                            'txid' => $op_result['txid'], 
-                        );
-                        $where = array(
-                            'u_c_l_id' => $results[$index]->u_c_l_id
-                        );
-                        $wpdb->update( $table, $data, $where );
-    
-                    }
-                }
-            }
-            
-            /** 
-             * view_mode header
-             */
-            $output  = '<h2>個人學習歷程</h2>';
-            $output .= '<form method="post">';
-            $output .= '<figure class="wp-block-table"><table><tbody>';
-            $output .= '<tr><td>'.'Name:'.'</td><td>'.get_orderdata($_id)->display_name.'</td></tr>';
-            $output .= '<tr><td>'.'Email:'.'</td><td>'.get_orderdata($_id)->order_email.'</td></tr>';
-            $output .= '</tbody></table></figure>';
-            //return $output;
-
-            /** 
-             * order relationship with course learnings
-             */
-            $course_header = true;
-            $output .= '<figure class="wp-block-table"><table><tbody>';
-            global $wpdb;
-            $results = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}order_course_learnings WHERE student_id = {$_id} ORDER BY course_id", OBJECT );
-            foreach ($results as $index => $result) {
-                if ($course_id == $results[$index]->course_id){$course_header=false;}
-                if ($course_header) {
-                    $course_id = $results[$index]->course_id;
-                    //$row = $wpdb->get_row( "SELECT * FROM {$wpdb->prefix}courses WHERE course_id = {$course_id}", OBJECT );
-                    //$output .= '<tr><td colspan="4">'.$row->course_title.'</td></td>';
-                    $product = wc_get_product( $results[$index]->course_id );
-                    $output .= '<tr><td colspan="4">'.$product->get_name().'</td></td>';
-                    $output .= '<tr><td>#</td><td>Learnings</td><td>Lecturer</td><td>Date</td><td>Witness</td><td>Date</td></tr>';
-                }
-
-                $lectureDate = wp_date( get_option( 'date_format' ), $results[$index]->lecture_date );
-                $certifidDate = wp_date( get_option( 'date_format' ), $results[$index]->certifid_date );
-                $output .= '<tr><td>'.$index.'</td>';
-                $learning_id = $results[$index]->learning_id;
-                $row = $wpdb->get_row( "SELECT * FROM {$wpdb->prefix}course_learnings WHERE learning_id = {$learning_id}", OBJECT );
-                $output .= '<td>'.$row->learning_title.'</td>';
-                $output .= '<td>'.get_orderdata($results[$index]->lecturer_id)->display_name.'</td>';
-                $output .= '<td><input type="text" name="_lecture_date_'.$index.'" value="'.$lectureDate.'">'.'</td>';
-                $output .= '<td>'.get_orderdata($results[$index]->witness_id)->display_name.'</td>';
-                $output .= '<td><input type="text" name="_certifid_date_'.$index.'" value="'.$certifidDate.'">'.'</td>';
-                $output .= '</tr>';
-
-            }
-            $output .= '</tbody></table></figure>';
-
-            /** 
-             * view_mode footer
-             */
-            $output .= '<div class="wp-block-buttons">';
-            $output .= '<div class="wp-block-button">';
-            $output .= '<input class="wp-block-button__link" type="submit" value="Submit" name="submit_action">';
-            $output .= '</div>';
-            $output .= '</form>';
-            $output .= '<form method="get">';
-            $output .= '<div class="wp-block-button">';
-            $output .= '<input class="wp-block-button__link" type="submit" value="Cancel"';
-            $output .= '</div>';
-            $output .= '</div>';
-            $output .= '</form>';
-
-            return $output;
-        }
-
         function edit_mode( $_id=null, $_mode ) {
 
             if ($_id==null){
-                $_id=get_current_order_id();
                 $_mode='Update';
             }
 
@@ -267,8 +43,14 @@ if (!class_exists('calendars')) {
             /** 
              * edit_mode
              */
+            global $wpdb;
+            $row = $wpdb->get_row( "SELECT * FROM {$wpdb->prefix}calendars WHERE event_id = {$_id}", OBJECT );
             $output  = '<form method="post">';
             $output .= '<figure class="wp-block-table"><table><tbody>';
+            $output .= '<tr><td>'.'Title:'.'</td><td><input style="width: 100%" type="text" name="_event_title" value="'.$row->event_title.'"></td></tr>';
+            $output .= '<tr><td>'.'Begin:'.'</td><td><input style="width: 100%" type="text" name="_event_begin" value="'.$row->event_begin.'"></td></tr>';
+            $output .= '<tr><td>'.'End:'.'</td><td><input style="width: 100%" type="text" name="_event_end" value="'.$row->event_end.'"></td></tr>';
+/*
             if( $_mode=='Update' ) {
                 $output .= '<tr><td>'.'Name:'.'</td><td><input style="width: 100%" type="text" name="_display_name" value="'.get_orderdata($_id)->display_name.'"></td></tr>';
                 $output .= '<tr><td>'.'Email:'.'</td><td><input style="width: 100%" type="text" name="_order_email" value="'.get_orderdata($_id)->order_email.'"></td></tr>';
@@ -279,10 +61,16 @@ if (!class_exists('calendars')) {
                 $output .= '<tr><td>'.'Name:'.'</td><td><input style="width: 100%" type="text" name="_display_name" value=""></td></tr>';
                 $output .= '<tr><td>'.'Email:'.'</td><td><input style="width: 100%" type="text" name="_order_email" value=""></td></tr>';
             }
+*/            
             $output .= '</tbody></table></figure>';
     
             $output .= '<div class="wp-block-buttons">';
             $output .= '<div class="wp-block-button">';
+            $output .= '<input class="wp-block-button__link" type="submit" value="Update" name="update_action">';
+            $output .= '</div>';
+            $output .= '<div class="wp-block-button">';
+            $output .= '<input class="wp-block-button__link" type="submit" value="Delete" name="delete_action">';
+/*            
             if( $_mode=='Update' ) {
                 //$output .= '<input class="wp-block-button__link" type="submit" value="Update" name="update_action">';
             } else if( $_mode=='Delete' ) {
@@ -290,10 +78,13 @@ if (!class_exists('calendars')) {
             } else {
                 //$output .= '<input class="wp-block-button__link" type="submit" value="Create" name="create_action">';
             }
+*/            
             $output .= '</div>';
+/*            
             $output .= '<div class="wp-block-button">';
             $output .= '<input class="wp-block-button__link" type="submit" value="Cancel"';
             $output .= '</div>';
+*/            
             $output .= '</div>';
             $output .= '</form>';
         
@@ -320,14 +111,14 @@ if (!class_exists('calendars')) {
              */
             global $wpdb;
             $user_id = get_current_user_id();
-            $results = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}my_calendars WHERE event_host = {$user_id}", OBJECT );
+            $results = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}calendars WHERE event_host = {$user_id}", OBJECT );
             $output  = '<h2>My Calendars</h2>';
             $output .= '<figure class="wp-block-table"><table><tbody>';
             $output .= '<tr><td>Title</td><td>Begin</td><td>End</td><td></td></tr>';
             foreach ( $results as $index=>$result ) {
                 //$output .= '<form method="post">';
                 $output .= '<tr>';
-                $output .= '<td><a href="?view_mode=true&_id='.$result->event_id.'">'.$result->event_title.'</a></td>';
+                $output .= '<td><a href="?edit_mode=true&_id='.$result->event_id.'">'.$result->event_title.'</a></td>';
                 $output .= '<td>'.$result->event_begin.'</td>';
                 $output .= '<td>'.$result->event_end.'</td>';
                 //$output .= '<input type="hidden" value="'.$product->get_id().'" name="_id">';
@@ -340,7 +131,7 @@ if (!class_exists('calendars')) {
             $output .= '<form method="get">';
             $output .= '<div class="wp-block-buttons">';
             $output .= '<div class="wp-block-button">';
-            $output .= '<input class="wp-block-button__link" type="submit" value="Create" name="edit_mode">';
+            //$output .= '<input class="wp-block-button__link" type="submit" value="Create" name="edit_mode">';
             //$output .= '<a class="wp-block-button__link" href="/checkout">Create</a>';
             $output .= '</div>';
             $output .= '<div class="wp-block-button">';
@@ -384,7 +175,7 @@ if (!class_exists('calendars')) {
             $charset_collate = $wpdb->get_charset_collate();
             require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
 
-            $sql = "CREATE TABLE `{$wpdb->prefix}my_calendars` (
+            $sql = "CREATE TABLE `{$wpdb->prefix}calendars` (
                 event_id int NOT NULL AUTO_INCREMENT,
                 event_begin int NOT NULL,
                 event_end int,
@@ -511,7 +302,7 @@ function enabling_date_picker() {
     // Load the datepicker jQuery-ui plugin script
     wp_enqueue_script( 'jquery-ui-datepicker' );
 }
-
+/*
 // Call datepicker functionality in your custom text field
 add_action('woocommerce_after_order_notes', 'my_custom_checkout_field', 10, 1);
 function my_custom_checkout_field( $checkout ) {
@@ -529,21 +320,7 @@ function my_custom_checkout_field( $checkout ) {
             $("#datepicker").datepicker();
         });
     </script>';
-/*
-    woocommerce_form_field(
-        'order_pickup_date', 
-        array(
-            'type'          => 'text',
-            'class'         => array('my-field-class form-row-wide'),
-            'id'            => 'datepicker',
-            'required'      => true,
-            'label'         => __('Delivery Date'),
-            'placeholder'   => __('Select Date'),
-            'options'       => $mydateoptions
-        ), 
-        $checkout->get_value( 'order_pickup_date' )
-    );
-*/
+
     $technician_options = array('roverchen','李光祥');
     woocommerce_form_field(
         'pickup_technician', 
@@ -593,5 +370,5 @@ function technician_options( $learning_id=null ) {
     }
     return $technician_options;
 }
-
+*/
 ?>
