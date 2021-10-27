87b5c0561f5063ce97f791c6e929e3f85dea21fc
roverchen <rover.k.chen@gmail.com>
2021-10-27 21:30:56 +0800

    $output .= '</tbody></table></figure>';

--- foot-reflexology.php
+++ foot-reflexology.php
@@ -17,7 +17,7 @@
 if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
-include_once dirname( __FILE__ ) . '/includes/class-badges.php';
+//include_once dirname( __FILE__ ) . '/includes/class-badges.php';
 include_once dirname( __FILE__ ) . '/includes/class-calendars.php';
 include_once dirname( __FILE__ ) . '/includes/class-orders.php';
 include_once dirname( __FILE__ ) . '/includes/class-courses.php';
