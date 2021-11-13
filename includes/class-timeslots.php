2739677549690ec8a7c8e6647a1b90021f6392fe
Rover Chen <rover.k.chen@gmail.com>
2021-11-13 15:30:57 +0800

    $_GET

--- includes/class-timeslots.php
+++ includes/class-timeslots.php
@@ -153,9 +153,9 @@ if (!class_exists('timeslots')) {
             }
 
             if( isset($_GET['edit_mode']) ) {
-                return $_GET['edit_mode'];
                 if ($_GET['edit_mode']=='Create') return self::edit_mode();
                 if ($_GET['edit_mode']=='true') return self::edit_mode( $_GET['_id'], $_GET['edit_mode'] );
+                return '$_GET:'.$_GET['edit_mode'];
             }            
 
             /**
