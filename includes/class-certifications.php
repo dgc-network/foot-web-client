b510d368eaf870cb66b84d1e832bd4b755527a1c
Rover Chen <rover.k.chen@gmail.com>
2021-11-15 12:15:31 +0800

    empty($row)

--- includes/class-certifications.php
+++ includes/class-certifications.php
@@ -131,10 +131,13 @@ if (!class_exists('certifications')) {
             //$output .= '<div style="display:flex">';
             global $wpdb;
             $results = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}timeslots ORDER BY timeslot_begin", OBJECT );
-            $output .= '<div style="text-align:center; width:100px">';
+            //$output .= '<div style="text-align:center; width:100px">';
+            $output .= '<div>';
             foreach ( $results as $index=>$result ) {
                 $row = $wpdb->get_row( "SELECT * FROM {$wpdb->prefix}available_timeslots WHERE available_host={$_id}", OBJECT );
-                $output .= '<input type="checkbox" name=""> '.$result->timeslot_begin.' ~ '.$result->timeslot_end.'<br>';
+                $output .= '<input type="checkbox" name="" ';
+                if (!empty($row)) {$output .= ' value="true" checked';}
+                $output .= '> '.$result->timeslot_begin.' ~ '.$result->timeslot_end.'<br>';
                 //$output .= '<div class="timepicker" style="margin:5px; border-style:solid; border-width:thin;">'.$result->timeslot_begin.'</div>';
             }
             $output .= '</div>';
