af38b614b12668cae7d019c84a1c17ae6e53f6b0
roverchen <rover.k.chen@gmail.com>
2021-11-17 22:40:01 +0800

    'admin-ajax.php'

--- includes/class-certifications.php
+++ includes/class-certifications.php
@@ -79,11 +79,12 @@ if (!class_exists('certifications')) {
 			                'whatever': 1234
                         }
 	    	            // since 2.8 ajaxurl is always defined in the admin header and points to admin-ajax.php
-                        jQuery.post(ajaxurl, data, function(response) {
+                        //jQuery.post(ajaxurl, data, function(response) {
+                        jQuery.post('admin-ajax.php', data, function(response) {                            
 	    		            alert('Got this from the server: ' + response);
     		            });
                     });
-                    
+
                     $('.timepicker').on({
                         mouseenter: function(){
                             $(this).css({"border-color":"red","color":"red","cursor":"pointer"});
