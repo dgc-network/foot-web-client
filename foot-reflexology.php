60e682be52c6c144707ed2b78e23c6f0f7ba84de
roverchen <rover.k.chen@gmail.com>
2022-07-06 20:21:26 +0800

    $course_page = new courses();

--- foot-reflexology.php
+++ foot-reflexology.php
@@ -17,30 +17,30 @@
 if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
+
 //include_once dirname( __FILE__ ) . '/line-bot-sdk-tiny/LINEBotTiny.php';
 //include_once dirname( __FILE__ ) . '/line-bot-sdk-tiny/echo_bot.php';
 
-//include_once dirname( __FILE__ ) . '/includes/class-calendars.php';
-//include_once dirname( __FILE__ ) . '/includes/class-badges.php';
-include_once dirname( __FILE__ ) . '/includes/class-orders.php';
-include_once dirname( __FILE__ ) . '/includes/class-courses.php';
-include_once dirname( __FILE__ ) . '/includes/class-certifications.php';
-//include_once dirname( __FILE__ ) . '/includes/class-users.php';
-include_once dirname( __FILE__ ) . '/vendor/autoload.php';
-include_once dirname( __FILE__ ) . '/blockchain/php-OP_RETURN/OP_RETURN.php';
-include_once dirname( __FILE__ ) . '/blockchain/php-OP_RETURN/op_return_setting.php';
-include_once dirname( __FILE__ ) . '/blockchain/build/gen/GPBMetadata/Payload.php';
-include_once dirname( __FILE__ ) . '/blockchain/build/gen/CreateCourseAction.php';
-include_once dirname( __FILE__ ) . '/blockchain/build/gen/UpdateCourseAction.php';
-include_once dirname( __FILE__ ) . '/blockchain/build/gen/CreateCourseLearingAction.php';
-include_once dirname( __FILE__ ) . '/blockchain/build/gen/UpdateCourseLearingAction.php';
-include_once dirname( __FILE__ ) . '/blockchain/build/gen/CreateUserCourseLearingAction.php';
-include_once dirname( __FILE__ ) . '/blockchain/build/gen/UpdateUserCourseLearingAction.php';
+//include_once dirname( __FILE__ ) . '/includes/class-orders.php';
+//include_once dirname( __FILE__ ) . '/includes/class-courses.php';
+//include_once dirname( __FILE__ ) . '/includes/class-certifications.php';
+
+//include_once dirname( __FILE__ ) . '/vendor/autoload.php';
+//include_once dirname( __FILE__ ) . '/blockchain/php-OP_RETURN/OP_RETURN.php';
+//include_once dirname( __FILE__ ) . '/blockchain/php-OP_RETURN/op_return_setting.php';
+//include_once dirname( __FILE__ ) . '/blockchain/build/gen/GPBMetadata/Payload.php';
+//include_once dirname( __FILE__ ) . '/blockchain/build/gen/CreateCourseAction.php';
+//include_once dirname( __FILE__ ) . '/blockchain/build/gen/UpdateCourseAction.php';
+//include_once dirname( __FILE__ ) . '/blockchain/build/gen/CreateCourseLearingAction.php';
+//include_once dirname( __FILE__ ) . '/blockchain/build/gen/UpdateCourseLearingAction.php';
+//include_once dirname( __FILE__ ) . '/blockchain/build/gen/CreateUserCourseLearingAction.php';
+//include_once dirname( __FILE__ ) . '/blockchain/build/gen/UpdateUserCourseLearingAction.php';
 
 /**
  * Register a custom menu page.
  */
-$course_page = new courses();
+
+//$course_page = new courses();
 
 function my_register_menu_page() {
 
